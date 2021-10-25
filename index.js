f9f6723e39e50d62b11d4ec810f6ebbb0506c204
js-designs-321 <jatni.321@gmail.com>
2021-10-25 20:17:51 +0530

    Set up

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 8000
 
 // this is a comment by gaurav
-// this is a comment by sujay 
+// this is a comment by sujay
+// this is a comment by jatin
 app.get('/', (req, res) => {
     res.send('Hello');
 })
