10d4665f5d2e76550e6c59d57241c4181e912a41
js-designs-321 <jatni.321@gmail.com>
2021-10-30 03:21:55 +0530

    Fix navbar mobile view

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+const navbarContent = document.getElementById("navbar-content-div");
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
@@ -8,9 +9,15 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    console.log(navbarContent);
+    //navbarContent.classList.remove(".navbar-content");
+    navbarContent.classList.add("navbar-content-margin");
 }
 
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}
+    console.log(navbarContent);
+    //navbarContent.classList.add(".navbar-content");
+    navbarContent.classList.remove("navbar-content-margin");
+} 
